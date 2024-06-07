1e8747963cdf181ee53db444cfdef34d0a590976
AqueleCaio <106452341+AqueleCaio@users.noreply.github.com>
2024-06-07 12:47:20 -0300

    envio de mensagem implementado

--- js/index.js
+++ js/index.js
@@ -1,35 +1,4 @@
-$(window).on('load',function(){
-  gsap.to('#loader',1,{y:"-100%"});
-  gsap.to('#loader',1,{opacity:0});
-  gsap.to('#loader',0,{display:"none",delay:1});
-  gsap.to('#header',0,{display:"block",delay:1})
-  gsap.to('#navigation-content',0,{display:"none"});
-  gsap.to('#navigation-content',0,{display:"flex",delay:1});
-})
-
-// Função que faz a cor parecer antes de mudar para outra tela
-$(function(){
-  $('.color-panel').on("click",function(e) {
-    e.preventDefault();
-    $('.color-changer').toggleClass('color-changer-active');
-  });
-
-  $('.colors a').on("click",function(e) {
-    e.preventDefault();
-    var attr = $(this).attr("title");
-    console.log(attr);
-    $('head').append('<link rel="stylesheet" href="css/'+attr+'.css">');
-  });
-});
-
-$(function(){
-     $('.menubar').on('click',function(){
-         gsap.to('#navigation-content',.6,{y:0});
-     })
-     $('.navigation-close').on('click',function(){
-        gsap.to('#navigation-content',.6,{y:"-100%"});
-    });
-  }); 
+$(document).ready(function() {
 
 $(function(){
     var TxtRotate = function(el, toRotate, period) {
@@ -89,122 +58,36 @@ $(function(){
         document.body.appendChild(css);
       };
 })
-$(function(){
 
-    $('#about-link').on('click',function(){
-      gsap.to('#navigation-content',0,{display:"none",delay:.7});
-      gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
-  gsap.to('#header',0,{display:"none"});
-gsap.to('#blog',0,{display:"none"});
-gsap.to('#portfolio',0,{display:"none"});
-   gsap.to('#breaker',0,{display:"block"});
-   gsap.to('#breaker-two',0,{display:"block",delay:.1});
-gsap.to('#contact',0,{display:"none"});
-   gsap.to('#breaker',0,{display:"none",delay:2});
-   gsap.to('#breaker-two',0,{display:"none",delay:2});
-   gsap.to('#about',0,{display:"block",delay:.7});
-   gsap.to('#navigation-content',0,{display:'flex',delay:2});
- })
- $('#contact-link').on('click',function(){
-   gsap.to('#navigation-content',0,{display:"none",delay:.7});
-   gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
-gsap.to('#header',0,{display:"none"});
-gsap.to('#about',0,{display:"none"});
-gsap.to('#blog',0,{display:"none"});
-gsap.to('#portfolio',0,{display:"none"});
-gsap.to('#breaker',0,{display:"block"});
-gsap.to('#breaker-two',0,{display:"block",delay:.1});
-gsap.to('#breaker',0,{display:"none",delay:2});
-gsap.to('#breaker-two',0,{display:"none",delay:2});
-gsap.to('#contact',0,{display:"block",delay:.7});
-gsap.to('#navigation-content',0,{display:'flex',delay:2});
-})
-$('#portfolio-link').on('click',function(){
-  gsap.to('#navigation-content',0,{display:"none",delay:.7});
-  gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
-gsap.to('#header',0,{display:"none"});
-gsap.to('#about',0,{display:"none"});
-gsap.to('#contact',0,{display:"none"});
-gsap.to('#blog',0,{display:"none"});
-gsap.to('#breaker',0,{display:"block"});
-gsap.to('#breaker-two',0,{display:"block",delay:.1});
-gsap.to('#breaker',0,{display:"none",delay:2});
-gsap.to('#breaker-two',0,{display:"none",delay:2});
-gsap.to('#portfolio',0,{display:"block",delay:.7});
-gsap.to('#navigation-content',0,{display:'flex',delay:2});
-})
-$('#home-link').on('click',function(){
-  gsap.to('#navigation-content',0,{display:"none",delay:.7});
-  gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
-gsap.to('#header',0,{display:"none"});
-gsap.to('#about',0,{display:"none"});
-gsap.to('#portfolio',0,{display:"none"});
-gsap.to('#contact',0,{display:"none"});
-gsap.to('#blog',0,{display:"none"});
-gsap.to('#breaker',0,{display:"block"});
-gsap.to('#breaker-two',0,{display:"block",delay:.1});
-gsap.to('#breaker',0,{display:"none",delay:2});
-gsap.to('#breaker-two',0,{display:"none",delay:2});
-gsap.to('#header',0,{display:"block",delay:.7});
-gsap.to('#navigation-content',0,{display:'flex',delay:2});
-})
-
-})
-$(function(){
- var body =  document.querySelector('body');
- var $cursor = $('.cursor')
-   function cursormover(e){
-    
-    gsap.to( $cursor, {
-      x : e.clientX ,
-      y : e.clientY,
-      stagger:.002
-     })
-   }
-   function cursorhover(e){
-    gsap.to( $cursor,{
-     scale:1.4,
-     opacity:1
-    })
-    
-  }
-  function cursor(e){
-    gsap.to( $cursor, {
-     scale:1,
-     opacity:.6
-    }) 
-  }
-  $(window).on('mousemove',cursormover);
-  $('.menubar').hover(cursorhover,cursor);
-  $('a').hover(cursorhover,cursor);
-  $('.navigation-close').hover(cursorhover,cursor);
-
-})
-
-$('#submit').click(() =>{
- 
-  //seta um intervalo de 10 segundos para manter o botão escrito enviado e depois volta ao normal
-  $('#submit').html('Enviado!');
+  $('#submit').click(() =>{
   
-  setTimeout(() => {
-    $('#submit').html('Enviar');
-  }, 20000)
-});
+    if ($('#name').val() == '' || $('#email').val() == '' || $('#message').val() == '') {
+      $('#submit').html('Enviar');
+    } else {
+      $('#submit').html('Enviado!');
 
-$('#myForm').on('submit', function(e) {
-        e.preventDefault();
+    }
 
-        $.ajax({
-            url : $(this).attr('action') || window.location.pathname,
-            type: "POST",
-            data: $(this).serialize(),
-            success: function (data) {
-                $("#myForm")[0].reset();
-                alert('Email enviado com sucesso!');
-            },
-            error: function (jXHR, textStatus, errorThrown) {
-                alert(errorThrown);
-            }
-        });
+
+    setTimeout(() => {
+      $('#submit').html('Enviar');
+    }, 20000)
+  });
+
+  $('#myForm').on('submit', function(e) {
+    e.preventDefault();
+
+    $.ajax({
+        url : $(this).attr('action') || window.location.pathname,
+        type: "POST",
+        data: $(this).serialize(),
+        success: function (data) {
+            $("#myForm")[0].reset();
+        },
+        error: function (jXHR, textStatus, errorThrown) {
+            
+        }
     });
+  });
 
+});
